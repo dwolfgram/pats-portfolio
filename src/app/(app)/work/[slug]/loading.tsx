d7fefdab96948ec735e95f27f41b5dfb91fbf097
Daniel Wolfgram <dan@Daniels-MacBook-Pro.local>
2024-06-10 01:07:56 +0200

    fix: change slug loading order

--- src/app/(app)/work/[slug]/loading.tsx
+++ src/app/(app)/work/[slug]/loading.tsx
@@ -5,15 +5,15 @@ import styles from './index.module.css'
 function LoadingPage() {
   return (
     <section className={styles.container}>
+      <div className={styles.imagesContainer}>
+        <div className={`${styles.featuredImageSkeleton} skeleton`} />
+      </div>
       <div className={styles.aboutProject}>
         <div className={`${styles.aboutSkeleton} skeleton`} />
         <br />
         {Array.from(Array(9).keys()).map((item) => {
           return <div className={`${styles.aboutSkeleton} skeleton`} key={item} />
         })}
-      </div>
-      <div className={styles.imagesContainer}>
-        <div className={`${styles.featuredImageSkeleton} skeleton`} />
       </div>
     </section>
   )
